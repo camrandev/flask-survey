a52b4476dc1e84e14d3f215c7b2c016bcbd3bcdb
Crynowecer0 <crynowecer11@gmail.com>
2023-04-20 19:03:13 -0400

    progress on /answer, start work on end page

--- app.py
+++ app.py
@@ -23,16 +23,43 @@ def start_button():
     flash("please fill out the answer")
     return redirect("/question/0")
 
-@app.get("/question/0")
-def question_0():
+@app.get("/question/<int:question_num>")
+def question_0(question_num):
     """get question 0"""
 
-    question = survey.questions[0]
-    print(survey.questions)
+    question = survey.questions[question_num]
+    # print(survey.questions)
 
     return render_template("question.html",question = question )
+
 @app.post("/answer")
 def answer():
     """answer for the question"""
+
+    # put answer in responses
+    answer = request.form.get('answer')
+    response.append(answer)
+
+
+    print(f"\n\n\ncurrent response: {response}\n\n\n")
+
+    if len(response) < 4:
+        flash("please fill out the answer")
+        return redirect(f"/question/{len(response)}")
+    else:
+        flash("thanks for completing the survey")
+        # end page -> 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
