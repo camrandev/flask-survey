37f2106a78085a0f0db674ed50ee765d92443a4f
Xiandi Pan <cindypan1807@gmail.com>
2023-04-20 16:25:55 -0400

    question/0

--- app.py
+++ app.py
@@ -7,3 +7,32 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+response = []
+
+@app.get("/")
+def user_home():
+    """home page"""
+
+    return render_template("survey_start.html")
+
+@app.post("/begin")
+def start_button():
+    """start button that will redirect the page to a question page"""
+
+    flash("please fill out the answer")
+    return redirect("/question/0")
+
+@app.get("/question/0")
+def question_0():
+    """get question 0"""
+
+    question = survey.questions[0]
+    print(survey.questions)
+
+    return render_template("question.html",question = question )
+@app.post("/answer")
+def answer():
+    """answer for the question"""
+
+
